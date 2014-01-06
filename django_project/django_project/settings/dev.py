87f7af2383d8ab2de41b0f65da08016635f6418d
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-01-06 18:46:26 -0500

    Made modular settings files for development and production, haven't edited manage.py yet though

---
+++ django_project/django_project/settings/dev.py
@@ -0,0 +1,3 @@
+from settings import *
+#Alter or add development-specific variables
+#Based on this: http://tech.yipit.com/2011/11/02/django-settings-what-to-do-about-settings-py/
---
+++ django_project/django_project/settings/prod.py
@@ -0,0 +1,3 @@
+from settings import *
+#Alter or add production-specific variables
+#Based on this: http://tech.yipit.com/2011/11/02/django-settings-what-to-do-about-settings-py/
