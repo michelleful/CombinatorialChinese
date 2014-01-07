e31c84a069d75d22af45d1f429bdde9842a4a5fa
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-01-07 14:17:36 -0500

    Created admin site for lessons app

--- django_project/lessons/admin.py
+++ django_project/lessons/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from lessons.models import Page, Text, Lesson
+
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+class TextAdmin(admin.ModelAdmin):
+    pass
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(Page)
+admin.site.register(Text)
+admin.site.register(Lesson)
